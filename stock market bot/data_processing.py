e8ac60c58e8a140f7e9d9d85b372a472931a3fb6
Ben Xiao <suyangxiao.ben@gmail.com>
2021-05-02 21:04:13 +0800

    IA update

--- stock market bot/data_processing.py
+++ stock market bot/data_processing.py
@@ -10,11 +10,15 @@ import os
 import csv
 import warnings
 from bs4 import BeautifulSoup
+
 from numpy import log as ln
+from numpy import random
+from scipy.stats import binom
 
 matplotlib.rcParams['figure.figsize'] = (10.0, 4.0)
 matplotlib.style.use('ggplot')
-
+# Global list of the tickers
+L = ["AAPL","TSLA","QCOM","GME","AMC"]
 
 def percentage_change(Open, Close):
     percent = ((Close - Open)/Open)*100
@@ -46,6 +50,11 @@ def t(x, u, d, N):
     for i in range (len(x)):
         x[i] = (ln(x[i])-N*ln(d))/(ln(u)-ln(d))
     return x
+
+
+def partial(p1, p2, a1, a2, b1, b2):
+    pass
+
 #checking the if the file is empty
 if os.stat('stock_symbols.txt').st_size==0:
 #Downloading symbols of all stocks from the website
@@ -73,6 +82,258 @@ if os.stat('stock_symbols.txt').st_size==0:
     np.savetxt('stock_symbols.txt', np.array(symbols_clean), fmt="%s")
 else:
     pass
+
+#IA specific, downloading data for stock tickers in L
+# temp is data for all the stocks over a fixed period
+# temp is for 6d 1m interval
+if os.stat('IA_data_1w_1m.csv').st_size==0:
+    for data in L:
+        temp = yf.download(tickers=data, period="7d", interval = "1m")
+        temp.drop(['High','Low','Adj Close','Volume'], axis=1)
+        temp['delta p'] = [percentage_change(x, y) for x, y in zip(temp['Open'], temp['Close'])]
+        temp.to_csv('IA_data_1w_1m.csv', mode='a', encoding='utf-8')
+else:
+    pass
+#temp 2 is for 1mo 5m interval
+if os.stat('IA_data_1mo_5m.csv').st_size==0:
+    for data in L:
+        temp2 = yf.download(tickers=data, period="1mo", interval = "5m")
+        temp2.drop(['High','Low','Adj Close','Volume'], axis=1)
+        temp2['delta p'] = [percentage_change(x, y) for x, y in zip(temp2['Open'], temp2['Close'])]
+        temp2.to_csv('IA_data_1mo_5m.csv', mode='a', encoding='utf-8')
+else:
+    pass
+
+
+
+#Modelling using binomial distribution
+hist_data = yf.download(tickers="GME", start="2018-02-10", end="2021-02-09", interval = "1d").drop(['High','Low','Adj Close','Volume'], axis=1) 
+hist_data2 = yf.download(tickers="LTC-USD", start="2018-02-10", end="2021-02-09", interval = "1d").drop(['High','Low','Adj Close','Volume'], axis=1)
+
+#GME
+change_col = [change(x, y) for x, y in zip(hist_data['Open'], hist_data['Close'])]
+hist_data['X'] = change_col
+sorted_col = np.sort(change_col, kind = 'mergesort')
+
+#Litecoin
+change_col2 = [change(x, y) for x, y in zip(hist_data2['Open'], hist_data2['Close'])]
+hist_data2['Y'] = change_col2
+sorted_col2 = np.sort(change_col2, kind = 'mergesort')
+
+#values for u and d 
+N = 390
+d_i = sorted_col[0]
+u_i =sorted_col[-1]
+d = sorted_col[0]**(1/N)
+u = sorted_col[-1]**(1/N)
+print(d)
+print(u)
+
+#litecoin calculation only
+d_i2 = sorted_col2[0]
+u_i2 =sorted_col2[-1]
+d2 = sorted_col2[0]**(1/N)
+u2 = sorted_col2[-1]**(1/N)
+print(d2)
+print(u2)
+
+#normal distribution graph
+y, x = np.histogram(change_col, bins=300, density=True)
+x = (x + np.roll(x, -1))[:-1] / 2.0
+params = st.norm.fit(change_col)
+arg = params[:-2]
+loc = params[-2]
+scale = params[-1]
+
+# Calculate fitted PDF and error with fit in distribution using SSE
+pdf = st.norm.pdf(x, loc=loc, scale=scale, *arg)
+sse = np.sum(np.power(y - pdf, 2.0))
+print(sse)
+print(params)
+
+#visualization
+pdf = make_pdf(st.norm, params)
+ax = pdf.plot(lw=2, label='PDF', legend=True)
+plt.hist(change_col, color = 'blue', density = True, edgecolor = 'black', bins=100)
+plt.xlim(0.5, 1.5)
+plt.show()
+
+bin = []
+for i in range(N+1):
+    bin.append(i)
+#Untransformed binomial distribution graph
+plt.hist(change_col, color = 'blue', density = True, stacked = True, edgecolor = 'black', bins=300)
+plt.xlabel("size the price is multiplied by")
+plt.ylabel("probability density")
+plt.title("Probability distribution of the size of the price multiplier of one trading day")
+
+plt.xlim(0.5, 1.5)
+
+plt.show()
+#GME: transformed binomial distribution graph
+t(change_col, u, d, N)
+mean = np.mean(change_col)
+prob = mean/N
+print(prob)
+#Kelly fraction
+f = prob/(1-d) + (1-prob)/(1-u)
+print(f)
+
+#Litecoin: transformed binomial distribution graph
+t(change_col2, u2, d2, N)
+mean2 = np.mean(change_col2)
+prob2 = mean2/N
+print(prob2)
+#Kelly fraction
+f2 = prob2/(1-d2) + (1-prob2)/(1-u2)
+print(f2)
+
+# transformed histogram
+plt.hist(change_col, color = 'blue', density = True, stacked = True, edgecolor = 'black', bins=bin)
+plt.xlabel("number of successes")
+plt.ylabel("probability density")
+plt.title("Probability distribution of the number of successes in one trading day")
+plt.xlim(0, 390)
+plt.show()
+
+# defining list of r values
+r_values = list(range(N + 1))
+# list of pmf values
+dist = [binom.pmf(r, N, prob) for r in r_values]
+# plotting the graph 
+plt.bar(r_values, dist, width= 1)
+plt.xlabel("number of successes")
+plt.ylabel("probability density")
+plt.title("Binomial distribution")
+plt.xlim(0, 390)
+plt.show()
+
+
+#Partial w.r.t. y coefficients
+print(prob*prob2*(u2-1))
+print((1-prob)*prob2*(u2-1))
+print(prob*(1-prob2)*(d2-1))
+print((1-prob)*(1-prob2)*(d2-1))
+#Partial w.r.t. x coefficients
+print(prob*prob2*(u-1))
+print((1-prob)*prob2*(d-1))
+print(prob*(1-prob2)*(u-1))
+print((1-prob)*(1-prob2)*(d-1))
+#data for graphing 3d function 
+print(prob*prob2)
+print((1-prob)*prob2)
+print(prob*(1-prob2))
+print((1-prob)*(1-prob2))
+print(u-1)
+print(d-1)
+print(u2-1)
+print(d2-1)
+
+#covariance part
+# Matching the dates 
+for i in range (len(hist_data)-3):
+    while hist_data.index[i] > hist_data2.index[i]:
+        hist_data2 = hist_data2.drop(hist_data2.index[i])
+    while hist_data.index[i] < hist_data2.index[i]:    
+        hist_data = hist_data.drop(hist_data.index[i])
+
+print(hist_data)
+print(hist_data2)
+
+X = [change(x, y) for x, y in zip(hist_data['Open'], hist_data['Close'])]
+Y = [change(x, y) for x, y in zip(hist_data2['Open'], hist_data2['Close'])]
+XY = np.stack((X, Y), axis=0)
+#variance for X
+M = np.cov(XY)
+var_x = M[0, 0]
+#Mean for x
+mean = np.mean(X)
+print(M)
+# for final section , sigma^2 and mu
+print(var_x)
+print(mean-1)
+
+# covarince for X' and Y'
+t(X, u, d, N)
+t(Y, u2, d2, N)
+X_pY_p = np.stack((X, Y), axis=0)
+#Cov
+#Bilinearity of covariance
+cov_M = np.cov(X_pY_p)/(N**2)
+cov_x_y = cov_M[0, 1]
+print(cov_x_y)
+#calculating a, b, c, and d
+a = cov_x_y + prob*prob2
+c = prob - a
+b = prob2 - a
+dd = 1-a-b-c
+print(a)
+print(b)
+print(c)
+print(dd)
+#Partial w.r.t. y coefficients (with covariance)
+print(a*(u2-1))
+print(b*(u2-1))
+print(c*(d2-1))
+print(dd*(d2-1))
+#Partial w.r.t. x coefficients (with covariance)
+print(a*(u-1))
+print(b*(d-1))
+print(c*(u-1))
+print(dd*(d-1))
+plt.scatter(X, Y)
+plt.xlabel("Gamestop")
+plt.ylabel("Litecoin")
+plt.show()
+
+
+"""
+np.savetxt('historical_data.txt', hist_data.values)
+hist_data.info()
+for label, content in hist_data.items():
+    print(f'label: {label}')
+    print(f'content: {content}', sep='\n')
+
+for i in range (len(percentage_col)):
+    percentage_col[i] = round(percentage_col[i], 2)
+
+"""
+#Reading data from stored txt file to start processing
+"""
+with open('historical_data.txt') as f:
+    pass
+    data = f.read()
+    print(data)
+"""
+"""
+1.For all strategies, do a 2 month 5m and 7 days 1m list 
+2. Update historical_data.txt everyday
+3. A system of reading and writing from the txt file
+"""
+"""
+    f = open("IA_data_1mo_5m.csv", "w")
+    f.truncate()
+    f.close()
+"""
+
+"""
+#compare against a proper binomial distribution 
+mean = np.mean(change_col)
+prob = mean/N
+fig, ax = plt.subplots(1, 1)
+n, p = N, prob
+mean, var, skew, kurt = binom.stats(n, p, moments='mvsk')
+x = np.arange(binom.ppf(0.00000000001, n, p),
+              binom.ppf(0.99999999999, n, p))
+ax.plot(x, binom.pmf(x, n, p), 'bo', ms=1, label='binom pmf')
+ax.vlines(x, 0, binom.pmf(x, n, p), colors='b', lw=5, alpha=0.5)
+plt.xlim(0, 390)
+
+plt.legend()
+plt.show()
+"""
+
+#Go through every single stock and calculate their percentage of increasing
 """
 with open('stock_symbols.txt') as f:
     with open('stock_data.csv', 'w') as p:
@@ -81,7 +342,7 @@ with open('stock_symbols.txt') as f:
             try:
                 with warnings.catch_warnings():
                     warnings.filterwarnings('ignore')
-
+                # temp is data for all the stocks over a fixed period 
                 temp = yf.download(tickers=data, period="7d", interval = "1m")
                 temp.drop(['High','Low','Adj Close','Volume'], axis=1)
                 temp['delta p'] = [percentage_change(x, y) for x, y in zip(temp['Open'], temp['Close'])]
@@ -96,74 +357,4 @@ with open('stock_symbols.txt') as f:
             
             except Exception:
                 pass
-"""
-   
-    
-
-
-aapl_hist_data = yf.download(tickers="AAPL", period ="MAX", interval = "1d")
-aapl_hist_data.drop(['High','Low','Adj Close','Volume'], axis=1)
-
-percentage_col = [percentage_change(x, y) for x, y in zip(aapl_hist_data['Open'], aapl_hist_data['Close'])]
-aapl_hist_data['delta p'] = percentage_col
-
-change_col = [change(x, y) for x, y in zip(aapl_hist_data['Open'], aapl_hist_data['Close'])]
-sorted_col = np.sort(change_col, kind = 'mergesort')
-
-N = 390
-d = sorted_col[0]**(1/N)
-u = sorted_col[-1]**(1/N)
-
-print(d)
-print(u)
-"""
-y, x = np.histogram(percentage_col, bins=150, density=True)
-x = (x + np.roll(x, -1))[:-1] / 2.0
-params = st.norm.fit(percentage_col)
-arg = params[:-2]
-loc = params[-2]
-scale = params[-1]
-
-# Calculate fitted PDF and error with fit in distribution using SSE
-pdf = st.norm.pdf(x, loc=loc, scale=scale, *arg)
-sse = np.sum(np.power(y - pdf, 2.0))
-
-print(sse)
-print(params)
-
-#visualization
-pdf = make_pdf(st.norm, params)
-ax = pdf.plot(lw=2, label='PDF', legend=True)
-plt.hist(aapl_hist_data['delta p'], color = 'blue', density = True, edgecolor = 'black', bins=100)
-
-plt.xlim(-20, 20)
-"""
-plt.hist(change_col, color = 'blue', density = True, stacked = True, edgecolor = 'black', bins=100)
-plt.show()
-
-t(change_col, u, d, N)
-print(np.mean(change_col))
-
-
-"""
-np.savetxt('historical_data.txt', aapl_hist_data.values)
-aapl_hist_data.info()
-for label, content in aapl_hist_data.items():
-    print(f'label: {label}')
-    print(f'content: {content}', sep='\n')
-
-for i in range (len(percentage_col)):
-    percentage_col[i] = round(percentage_col[i], 2)
-
-"""
-"""
-with open('historical_data.txt') as f:
-    pass
-    data = f.read()
-    print(data)
-"""
-"""
-1.For all strategies, do a 2 month 5m and 7 days 1m list 
-2. Update historical_data.txt everyday
-3. A system of reading and writing from the txt file
 """
--- stock market bot/stock_market_bot.py
+++ stock market bot/stock_market_bot.py
@@ -4,28 +4,58 @@ import requests
 import alpaca_trade_api as tradeapi
 import data_processing
 import time
+import matplotlib.pyplot as plt
+import yfinance as yf
 
 from datetime import datetime, timedelta
 from pytz import timezone
+from data_processing import f
 api_time_format = '%Y-%m-%dT%H:%M:%S.%f-04:00'
 
-class bots:
-    def __init__ (self):
-        pass
+class portfolio:
+    def __init__ (self, f1, f2): #can use *args for larger portfolios 
+        self.portfolio = {
+            "safe" : 100000.0 ,
+            "p1" : 0.0,
+            "p2" : 0.0
+            }
+
+        self.value = self.portfolio["safe"] + self.portfolio["p1"]
+        self.f = [f1, f2]# self.f = list().append(f for f in *kelly)
+        self.portfolio["p1"] = f1*self.value
+        self.portfolio["p2"] = f2*self.value
+        self.portfolio["safe"] -= (self.portfolio["p1"]+self.portfolio["p2"])
+        self.last_close = 1 #self.lastclose = list().append(0 for i in range (len(kelly)))
+        self.last_close2 = 1
+
+    def adjust(self, open, close, open2, close2): #can use *args for larger portfolios 
+        # value change after one bet/timeframe
+        self.portfolio["p1"] *= close/self.last_close
+        if close2 != 0:
+            self.portfolio["p2"] *= close2/self.last_close2
+        # This is the update part, after recalculating the proportions
+        val = (self.portfolio["p1"] + self.portfolio["p1"] + self.portfolio["safe"])
+        self.portfolio["p1"] = self.f[0]*val
+        self.portfolio["p2"] = self.f[1]*val
+        self.portfolio["safe"] = (1 -self.f[1]-self.f[0])*val
+        self.last_close = close
+        self.last_close2 = close2
+        self.value = val
+        return self.value 
 
     def momentum_strat(self):
         pass
 
-    def stupid(self):
+    def Volatility (self):
         pass
 
-    def kelly_strat(self):
-        pass
 
-api = tradeapi.REST('PKINQZPKOQAV61LVM8OE', 'HLf9tdXqnrMbSwpqDgQsXMl9ql7Da0jlwSP77zMd', base_url='https://paper-api.alpaca.markets') # or use ENV Vars shown below
+#Public and private key to access account
+api = tradeapi.REST('PKRWAYC25MT5QMCVMEU3', '9vQ9DyjhDYy3MOrCArNmzZJPP84BftjYrXuZl1IW', base_url='https://paper-api.alpaca.markets') 
 account = api.get_account()
 positions = api.list_positions()
 
+#Sending trades to alpaca api to execute order
 """
 api.submit_order(symbol='QCOM',
     side='buy',
@@ -37,16 +67,52 @@ api.submit_order(symbol='QCOM',
         limit_price='305.0',
     ),
     stop_loss=dict(
-        stop_price='160.0',
-        limit_price='160.0',
+        stop_price='100.0',
+        limit_price='100.0',
     ))
-
 """
 
+#One stock
+bot = portfolio(0.24466950097405515, 0)
+df = pd.read_csv('IA_data_1w_1m.csv')
+gme = df[8189:10913].apply(pd.to_numeric, errors='ignore') #AAPL 1794, 2728, QCOM 5460 - 8186, GME 8189 -- 10913, GME month 5387 -- 7179
+#Creating a value column for the value of the portfolio when kelly strategy is not applied
+bot.last_close = gme['Open'][8189]
+gme['pval'] = [(val/gme['Open'][8189])*bot.value for val in gme['Close']]
+gme['pval'].plot(label='Value without optimization')
 
-with open('stock_data.csv') as f:
-    bot1=bots()
-    bot1.kelly_strat
+#Creating a value column for when the kelly strategy is applied
+gme['val'] = [bot.adjust(open, close, 0, 0) for open, close in zip(gme['Open'], gme['Close'])]
+gme['val'].plot(label='Value with optimization')
 
 
+plt.xlabel("time(minutes)")
+plt.ylabel("Value($)")
+plt.title("Value of portfolio in investing in Gamestop stock")
+
+plt.legend()
+plt.show()
+
+#two stocks
+bot2 = portfolio(0.2466949, 0.55996150) #no cov
+bot2_cov = portfolio(0.2445086, 0.5598149)#considers cov
+gme_df = yf.download(tickers="GME", start="2021-04-26", end="2021-04-30", interval = "1m").apply(pd.to_numeric, errors='ignore')
+lite_df = yf.download(tickers="LTC-USD", start="2021-04-26", end="2021-04-30", interval = "1m").apply(pd.to_numeric, errors='ignore')
+#matching time for data
+for i in range (len(gme_df)):
+    while gme_df.index[i] > lite_df.index[i]:
+        lite_df = lite_df.drop(lite_df.index[i])
+#set up
+bot2.last_close = gme_df['Open'][0]
+bot2.last_close2 = lite_df['Open'][0]
+bot2_cov.last_close = bot2.last_close
+bot2_cov.last_close2 = bot2.last_close2 
+#create value colomns (doesn't work yet)
+gme['nocov']  = [bot2.adjust(open, close, open2, close2) for open, close, open2, close2 in zip(gme_df['Open'], gme_df['Close'], lite_df['Open'], lite_df['Close'])]
+gme['cov'] = [bot2_cov.adjust(open, close, open2, close2) for open, close, open2, close2 in zip(gme_df['Open'], gme_df['Close'], lite_df['Open'], lite_df['Close'])]
+#plotting
+gme['nocov'].plot(label='Not considering covariance')
+gme['cov'].plot(label='Considering covariance')
+plt.legend()
+plt.show()
 
